981f61efa8f6cc505e05484e42c86c3caa234db7
skr-g16 <fahmirty@gmail.com>
2024-12-31 18:08:20 +0700

    fix some bugs

--- src/NotesServices.js
+++ src/NotesServices.js
@@ -6,7 +6,7 @@ class NoteServices {
 
   async getNotes(userId) {
     const query = {
-      text: `SELECT notes.* from notes
+      text: `SELECT notes.* FROM notes
       LEFT JOIN collaborations ON collaborations.note_id = notes.id
       WHERE notes.owner = $1 OR collaborations.user_id = $1
       GROUP BY notes.id`,
